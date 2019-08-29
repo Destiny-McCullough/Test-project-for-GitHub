966d61b024aa52ae10e3259b5f545fefa341543e
Destiny McCullough <destinymccullough99@gmail.com>
2019-08-29 15:34:10 -0500

    i added 2 comments.

--- Test project for GitHub/ViewController.swift
+++ Test project for GitHub/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+// I added a comment
+// I added 2 comments
 }
 
